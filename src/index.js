85a2ef2ec8ca75ad97e87b8b50f1a2be6d52b948
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-09-09 03:58:29 +0600

    added solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((accum, curr) => {
+    return curr < accum ? curr : accum;
+  });
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((accum, curr) => {
+    return curr > accum ? curr : accum;
+  });
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((accum, curr) => {
+    return (accum + curr);
+  }) / array.length;
 }
